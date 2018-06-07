323fc45a9e6c05708b4f7e46e2aec3ed5c0adbd5
Marco MOSCARDINI <marco@marcomoscardini-ubuntu-desktop.gac-technology.local>
2018-06-07 11:50:09 +0200

    fixed namespaces

--- GacModelBundle.php
+++ GacModelBundle.php
@@ -6,7 +6,7 @@
  * and open the template in the editor.
  */
 
-namespace Gac\Bundle\ModelBundle;
+namespace gacmodelbundle\Gac\Bundle\ModelBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
